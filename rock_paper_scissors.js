469c1dc22bce93ae2a44e4a343f3c705efbb21f9
tw202 <thomas_wiese08@web.de>
2023-04-09 10:34:05 +0200

    add checking for winner case = rock

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,6 +1,7 @@
 function getComputerChoice(){
     let computerChoice;
     let randomNumber;
+    let isDraw;
     randomNumber = Math.floor(Math.random() * 3) + 1;
 
     switch(randomNumber){
@@ -17,5 +18,32 @@ function getComputerChoice(){
 
     return computerChoice;
 }
-let currentComputerChoice = getComputerChoice();
-console.log(currentComputerChoice);
+
+function playRound(playerSelection, computerSelection){
+    let endGameMessage;
+    let playerSelectionLowercase = playerSelection.toLowerCase();
+    let computerSelectionLowerCase = computerSelection.toLowerCase();
+    let winOrLose;
+    switch (playerSelectionLowercase){
+        case "rock":
+            if (computerSelectionLowerCase == "paper"){
+                winOrLose = "lose";
+            }else if(computerSelectionLowerCase == "scissors"){
+                winOrLose = "win";
+            }else {
+                isDraw = true;
+            }
+            break;
+        case "paper":
+            break;
+        case "scissors":
+            break;
+    }
+
+    endGameMessage = `You ${winOrLose}! ${winner} beats ${loser}`;
+    return endGameMessage;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
